b3831689700a37f581ba8d22e9bb5835705f4716
Gabriel Santos <gabrielsantos@ext.cvccorp.com.br>
2019-04-17 14:44:03 -0300

    initial structure

--- src/app.js
+++ src/app.js
@@ -1,14 +1,34 @@
 'use strict'
 
-import React from 'react'
+import React, { Component } from 'react'
 
 import './css/style.css'
 
-const App = () => (
-  <div className='editor'>
-    <textarea />
-    <div className='view' />
-  </div>
-)
+class App extends Component {
+  constructor() {
+    super()
+
+    this.handleSubmit = (e) => {
+      e.preventDefault();
+      this.setState({value: e.target.textarea.value})
+    }
+
+    this.state = {
+      value: ''
+    }
+  }
+
+  render() {
+    return (
+      <div className='editor'>
+        <form onSubmit={this.handleSubmit}>
+          <textarea name='textarea' />
+          <button type='submit'>Renderizar</button>
+        </form>
+        <div className='view'>{this.state.value}</div>
+      </div>
+    )
+  }
+}
 
 export default App
